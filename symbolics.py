5eb6d30fbbc4c2e66753dd2e5c63167a381fe044
Christopher Albert <albert@alumni.tugraz.at>
2023-11-12 19:04:38 +0100

    Last initial version

--- symbolics.py
+++ symbolics.py
@@ -1,7 +1,7 @@
 #%%
 import numpy as np
 from sympy import symbols, simplify, diff
-from sympy.vector import CoordSys3D, curl, gradient
+from sympy.vector import CoordSys3D, divergence, curl, gradient
 from sympy.plotting import plot3d
 from spb import *
 
@@ -13,23 +13,31 @@ N = CoordSys3D('N')
 i, j, k = N.base_vectors()
 x, y, z = N.base_scalars()
 
-# Define B0 and A as functions
-Bmod = 1.0 + z**2
-A = Bmod*(-0.5 * y * i + 0.5 * x * j)
+# Magnetic bottle
+Bscale = 1.0 + z**2
 
-# Calculate B as the curl of A and simplify
+# Uniform field
+#Bscale = 1.0
+
+#%%
+A = Bscale*(-0.5 * y * i + 0.5 * x * j)
+A
+#%%
 B = curl(A)
+B
+#%%
 Bmod = B.magnitude()
+Bmod
+#%%
 h = B.normalize()
+h
+#%% Basis vector field
 e1 = h.cross(A.normalize()).simplify()
-# %%
+e1
 
-# Define the range for the variables (e.g., -1 to 1)
-x_range = (-1, 1)
-y_range = (-1, 1)
-z_range = (-1, 1)
-
-# Create a 3D vector field plot
-plot = vector_field_3d(A, (x, *x_range), (y, *y_range), (z, *z_range), title='3D Vector Field', xlabel='X', ylabel='Y', zlabel='Z', show=False)
+#%%
+curl(e1).simplify()
+#%%
+divergence(e1).simplify()
 
 # %%
